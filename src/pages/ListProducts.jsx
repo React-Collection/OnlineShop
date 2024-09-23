3f315ea7fc46896b416d0a35d7f3b536c9353a56
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-24 01:52:17 +0330

    Feat(src): List products Component pages for user navigation across different pages using the react-router-dom library. All pages are also visible in the site's URL.

---
+++ src/pages/ListProducts.jsx
@@ -0,0 +1,16 @@
+import {Link } from "react-router-dom";
+
+import styles from "./pageStyles/listProduct.module.scss";
+import dataProducts from "../../dataBase/dataBase";
+
+const ListProducts = () => {
+  return (
+    <div className={styles.parent}>
+      {dataProducts.map((product) => (
+        <Link to="/Products" key={product.id}>{product.name}</Link>
+      ))}
+    </div>
+  );
+};
+
+export default ListProducts;
