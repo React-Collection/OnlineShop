403f6757245e8bd854de8d3664c97e9d43243cde
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-24 01:58:01 +0330

    Feat(src): app page This is the main page where all the components are located. Links are created through this page.

---
+++ src/pages/App.jsx
@@ -0,0 +1,52 @@
+import { Route, Routes } from "react-router-dom";
+import "/styles/cssReset.css";
+import "/styles/style.scss";
+
+import HeaderComponent from "../../components/headerComponent/HeaderComponent";
+import styles from "./pageStyles/index.module.scss";
+import NotFoundPage from "./NotFoundPage";
+import ListProducts from "./ListProducts";
+import ShopPageComponent from "./Shop";
+import HomePageComponent from "./Home";
+import ContactUs from "./ContactUs";
+import Products from "./Products";
+import About from "./About";
+
+function App() {
+  return (
+    <div className={styles.bgSection}>
+      <HeaderComponent />
+      <div className={styles.innerBackground}>
+        <Routes>
+          <Route path="/Home" element={<HomePageComponent />}></Route>
+          <Route
+            path="/Shop"
+            element={<ShopPageComponent />}
+          ></Route>
+          <Route
+            path="/About"
+            element={<About />}
+          ></Route>
+          <Route
+            path="/ContactUs"
+            element={<ContactUs />}
+          ></Route>
+          <Route
+            path="*"
+            element={<NotFoundPage />}
+          ></Route>
+          <Route
+            path="/ListProducts"
+            element={<ListProducts />}
+          ></Route>
+          <Route
+            path="/Products"
+            element={<Products />}
+          ></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
