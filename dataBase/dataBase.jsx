dd248f9d745896c1e9f62d1b1819a502d1bd8691
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-24 02:01:31 +0330

    Feat(dataBase): data base All information and data are stored here. The structure is in the form of an array and an object.

---
+++ dataBase/dataBase.jsx
@@ -0,0 +1,52 @@
+const dataProducts = [
+  {
+    id:1,
+    name: "Sun glasses",
+    price: "190$",
+    description: "This is a Sun Glasses",
+    image: "https://picsum.photos/200/300",
+    category: "Accessory",
+  },
+  {
+    id:2,
+    name: "Laptop",
+    price: "2000$",
+    description: "This is a Laptop",
+    image: "https://picsum.photos/200/300",
+    category: "Technology",
+  },
+  {
+    id:3,
+    name: "Cup",
+    price: "50$",
+    description: "This is a cup",
+    image: "https://picsum.photos/200/300",
+    category: "Gift",
+  },
+  {
+    id:4,
+    name: "Book",
+    price: "35$",
+    description: "This is a Book",
+    image: "https://picsum.photos/200/300",
+    category: "Books",
+  },
+  {
+    id:5,
+    name: "Dress",
+    price: "50$",
+    description: "This is a Dress",
+    image: "https://picsum.photos/200/300",
+    category: "Clothing",
+  },
+  {
+    id:6,
+    name: "Doll",
+    price: "65$",
+    description: "This is a Doll",
+    image: "https://picsum.photos/200/300",
+    category: "Kids",
+  },
+];
+
+export default dataProducts;
