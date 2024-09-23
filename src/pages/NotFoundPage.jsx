6d32ad309e53b5d900c087f722f0941a4d80ab18
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-24 01:53:03 +0330

    Feat(src): 404 page Component pages for user navigation across different pages using the react-router-dom library. All pages are also visible in the site's URL.

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import styles from "./pageStyles/notFoundPage.module.scss";
+
+const NotFoundPage = ()=> {
+  return (
+    <div className={styles.parentBg}>
+
+    </div>
+  )
+}
+
+export default  NotFoundPage
