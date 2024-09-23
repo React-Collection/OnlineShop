0ceb58f5e50b4cd356892b5defc4e5cdf2339f22
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-24 01:52:55 +0330

    Feat(src): shop page Component pages for user navigation across different pages using the react-router-dom library. All pages are also visible in the site's URL.

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,14 @@
+import ListProducts from "./ListProducts";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>All Products</h1>
+      <div>
+        <ListProducts />
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
