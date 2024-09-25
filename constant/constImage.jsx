1d0c654282f3cb03c852f852ec7833a1d2200cd2
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-25 03:41:27 +0330

    Feat(constant): const images Photos changed in fixed variables. or magic variable. So that the data can be used more easily.

--- constant/constImage.jsx
+++ constant/constImage.jsx
@@ -1,1 +1,3 @@
 export const LOGO = "/public/images/logo/maskgroup.png"
+export const ABOUT = "/public/images/aboutPage/Mr_ Nomad.jpg"
+export const MANAGEMENT = "/public/images/aboutPage/cc_Brandon_Instagram-featured-186x198.webp"
