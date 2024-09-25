5c9d4fabf5764d4990d00570d87ae3f4d97c5464
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-25 03:37:19 +0330

    Feat(pages): app page add other links routers.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -19,30 +19,13 @@ function App() {
       <div className={styles.innerBackground}>
         <Routes>
           <Route path="/Home" element={<HomePageComponent />}></Route>
-          <Route
-            path="/Shop"
-            element={<ShopPageComponent />}
-          ></Route>
-          <Route
-            path="/About"
-            element={<About />}
-          ></Route>
-          <Route
-            path="/ContactUs"
-            element={<ContactUs />}
-          ></Route>
-          <Route
-            path="*"
-            element={<NotFoundPage />}
-          ></Route>
-          <Route
-            path="/ListProducts"
-            element={<ListProducts />}
-          ></Route>
-          <Route
-            path="/Products"
-            element={<Products />}
-          ></Route>
+          <Route path="/Shop" element={<ShopPageComponent />}></Route>
+          <Route path="/About" element={<About />}></Route>
+          <Route path="/ContactUs" element={<ContactUs />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
+          <Route path="/ListProducts" element={<ListProducts />}></Route>
+          <Route path="/Shop/:id" element={<ListProducts />}></Route>
+          <Route path="/Products" element={<Products />}></Route>
         </Routes>
       </div>
     </div>
