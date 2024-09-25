80675cd1becb4e7b73a1716b44af9d1da9111c0e
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-25 03:36:48 +0330

    Feat(pages):Products add card component for show products and read them by map.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,21 @@
+import CardComponent from "../../components/cardComponent/CardComponent";
+import styles from "./pageStyles/products.module.scss";
+import dataProducts from "../../dataBase/dataBase";
+
 const Products = () => {
   return (
-<>
-<h1>hi</h1>
-
-</>
-  )
+    <div className={styles.parentSection}>
+      {dataProducts.map((product) => (
+        <CardComponent
+          key={product.id}
+          banner={product.image}
+          title={product.name}
+          description={product.description}
+          price={product.price}
+          category={product.category}
+        />
+      ))}
+    </div>
+  );
 };
 export default Products;
