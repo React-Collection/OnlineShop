bda985ef39cd7ca451591f518529910879da18d2
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-10-10 22:17:50 +0330

    Fixed(pages): shop page Outlet is a place where components related to sub-routes are displayed.

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,15 +1,12 @@
-// import { useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import styles from "./pageStyles/shop.module.scss";
-import ListProducts from "./ListProducts";
 
 const Shop = () => {
   return (
     <div className={styles.parentSection}>
-      <h1>All Products </h1>
-      <div>
-        <ListProducts />
-      </div>
+      <h1>All Products</h1>
+      <Outlet />
     </div>
   );
 };
