958c7e27910fde8ebc7f8403ddfdd6283f46b904
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-10-10 22:15:08 +0330

    Fixed(pages): app.jsx Setting up Nested Routes for navigation on the product page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,13 +3,13 @@ import "/styles/cssReset.css";
 import "/styles/style.scss";
 
 import HeaderComponent from "../../components/headerComponent/HeaderComponent";
+import ProductDetailWrapper from "./ProductDetailWrapper";
 import styles from "./pageStyles/index.module.scss";
 import NotFoundPage from "./NotFoundPage";
 import ListProducts from "./ListProducts";
 import ShopPageComponent from "./Shop";
 import HomePageComponent from "./Home";
 import ContactUs from "./ContactUs";
-import Products from "./Products";
 import About from "./About";
 
 function App() {
@@ -19,13 +19,14 @@ function App() {
       <div className={styles.innerBackground}>
         <Routes>
           <Route path="/Home" element={<HomePageComponent />}></Route>
-          <Route path="/Shop" element={<ShopPageComponent />}></Route>
+          <Route path="/Shop" element={<ShopPageComponent />}>
+            {/* Nested Routes */}
+            <Route index element={<ListProducts />} />
+            <Route path=":id" element={<ProductDetailWrapper />} />
+          </Route>
           <Route path="/About" element={<About />}></Route>
           <Route path="/ContactUs" element={<ContactUs />}></Route>
           <Route path="*" element={<NotFoundPage />}></Route>
-          <Route path="/ListProducts" element={<ListProducts />}></Route>
-          <Route path="/Shop/:id" element={<ListProducts />}></Route>
-          <Route path="/Products" element={<Products />}></Route>
         </Routes>
       </div>
     </div>
